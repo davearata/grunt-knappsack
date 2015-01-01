cb6c7950721d10f8beb1f59d1432957836793667
Kevin Massaroni <kevinmassaroni@gmail.com>
2015-01-01 00:50:02 -0500

    making real test assertions

--- test/knappsack_test.js
+++ test/knappsack_test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var grunt = require('grunt');
+var ksackUpload = require('../js/knappsack-upload');
+var nock = require('nock');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -29,8 +30,43 @@ exports.test = {
   },
 
   default_options: function(test) {
-    test.expect(1);
-    test.ok(global.nockScope.isDone());
-    test.done();
+    test.expect(4);
+
+    var accessTokenRequestPath = '/oauth/token?client_id=mobile_api_client&client_secret=kzI7QNsbne8KOlS&grant_type=password&username=testuser&password=testpwd'
+    var nockAccessTokenScope = nock('http://mydomain.com:123')
+      .get(accessTokenRequestPath).reply(200, {access_token: 'test-access-token'});
+
+    var appVersionsQueryPath = '/api/v1/applications/testappid/applicationVersions';
+    var nockVersionQueryScope = nock('http://mydomain.com:123',{
+      reqheaders: {
+        'Authorization': 'Bearer test-access-token'
+      }
+    }).get(appVersionsQueryPath).reply(200, [{versionName: '1.0'}]);
+
+    var uploadPostPath = '/api/v1/applicationVersions';
+    var nockUploadScope = nock('http://mydomain.com:123')
+      .post(uploadPostPath).reply(200);
+
+    var options = {
+      knappsackHost: 'http://mydomain.com',
+      applicationId: 'testappid',
+      knappsackPort: 123,
+      knappsackPath: '',
+      file: 'test/fixtures/testing',
+      versionName: '1.0',
+      appState: 'GROUP_PUBLISH',
+      recentChanges: '',
+      username: 'testuser',
+      password: 'testpwd' };
+
+    function done(status) {
+      test.ok(status !== false, 'done status');
+      test.ok(nockAccessTokenScope.isDone(), 'http access token request');
+      test.ok(nockVersionQueryScope.isDone(), 'http version query');
+      test.ok(nockUploadScope.isDone(), 'upload post');
+      test.done();
+    }
+
+    ksackUpload(options, done);
   }
 };
